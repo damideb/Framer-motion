8b1d2b1200bc29d574e6f88bac5e4c15d12aa73d
Damilola Adebowale <adebzdamzy@gmail.com>
2024-07-20 23:33:48 +0100

    animatePresence

--- animation/src/components/Home.jsx
+++ animation/src/components/Home.jsx
@@ -2,6 +2,19 @@ import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
 const Home = () => {
+
+  const buttonVariant = {
+    hover:{
+      scale:1.1,
+      textShadow:'0px 0px 8px rgb(255,255,255)',
+      boxShadow:'0px 0px 8px rgb(255,255,255)',
+      transition:{
+        repeat:Infinity,
+        duration:1 // works just like keyframes. It transitions the scale to 1.1 infinitely. it can be set to whatever time you want it to run for
+
+      }
+    }
+  }
   return (
     <div className="home container">
       <motion.h2 initial={{opacity:0}}
@@ -11,7 +24,9 @@ const Home = () => {
         Welcome to Pizza Joint
       </motion.h2>
       <Link to="/base">
-        <motion.button animate={{scale:1.5}} whileHover={{scale:1.1}}>
+        <motion.button 
+        variants={buttonVariant}
+ whileHover='hover'>
           Create Your Pizza
         </motion.button>
       </Link>
