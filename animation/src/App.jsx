6bd33b007a01185fec5ea04dbeed9aff41efb77e
Damilola Adebowale <adebzdamzy@gmail.com>
2024-09-01 22:44:16 +0100

    final commit

--- animation/src/App.jsx
+++ animation/src/App.jsx
@@ -1,32 +1,47 @@
-import { useState } from 'react';
+import { useState, useEffect } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
-import Header from './components/Header';
-import Home from './components/Home';
-import Base from './components/Base';
-import Toppings from './components/Toppings';
-import Order from './components/Order';
-import { AnimatePresence } from 'framer-motion';
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Base from "./components/Base";
+import Toppings from "./components/Toppings";
+import Order from "./components/Order";
+import { AnimatePresence } from "framer-motion";
 import Modal from "./components/Modal";
-
 
 function App() {
   const [pizza, setPizza] = useState({ base: "", toppings: [] });
   const [showModal, setShowModal] = useState(false);
-  const location = useLocation()
+  const location = useLocation();
+
+  const fetchUsers = async () => {
+    try {
+      const data = await fetch("https://jsonplaceholder.typicode.com/todos/");
+
+      const res = await data.json();
+
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
   const addBase = (base) => {
-    setPizza({ ...pizza, base })
-  }
-  
+    setPizza({ ...pizza, base });
+  };
+
   const addTopping = (topping) => {
     let newToppings;
-    if(!pizza.toppings.includes(topping)){
+    if (!pizza.toppings.includes(topping)) {
       newToppings = [...pizza.toppings, topping];
     } else {
-      newToppings = pizza.toppings.filter(item => item !== topping);
+      newToppings = pizza.toppings.filter((item) => item !== topping);
     }
     setPizza({ ...pizza, toppings: newToppings });
-  }
+  };
 
   return (
     <>
@@ -44,7 +59,10 @@ function App() {
             element={<Toppings addTopping={addTopping} pizza={pizza} />}
           />
 
-          <Route path="/order" element={<Order pizza={pizza}  setShowModal={setShowModal}/>} />
+          <Route
+            path="/order"
+            element={<Order pizza={pizza} setShowModal={setShowModal} />}
+          />
 
           <Route path="/" element={<Home />} />
         </Routes>
--- animation/src/components/Home.jsx
+++ animation/src/components/Home.jsx
@@ -1,21 +1,19 @@
-import { Link } from 'react-router-dom';
-import { motion } from 'framer-motion';
-import Loader from './Loader';
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import Loader from "./Loader";
 
 const Home = () => {
-
   const buttonVariant = {
-    hover:{
-      scale:1.1,
-      textShadow:'0px 0px 8px rgb(255,255,255)',
-      boxShadow:'0px 0px 8px rgb(255,255,255)',
-      transition:{
-        repeat:Infinity,
-        duration:1 // works just like keyframes. It transitions the scale to 1.1 infinitely. it can be set to whatever time you want it to run for
-
-      }
-    }
-  }
+    hover: {
+      scale: 1.1,
+      textShadow: "0px 0px 8px rgb(255,255,255)",
+      boxShadow: "0px 0px 8px rgb(255,255,255)",
+      transition: {
+        repeat: Infinity,
+        duration: 1, // works just like keyframes. It transitions the scale to 1.1 infinitely. it can be set to whatever time you want it to run for
+      },
+    },
+  };
   const containerVariants = {
     hidden: {
       opacity: 0,
@@ -44,9 +42,9 @@ const Home = () => {
           Create Your Pizza
         </motion.button>
       </Link>
-      <Loader/>
+      <Loader />
     </motion.div>
   );
-}
+};
 
 export default Home;
